e396c3b69be999fd33d0ad8ed17d5e446bb34335
mali1998 <mali@arconme.com>
2024-07-28 18:16:22 +0300

    Gun loaded and works with bullet

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private StarterAssetsInputs _input;
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private GameObject bulletPoint;
+    [SerializeField]
+    private float bulletSpeed = 600;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _input = transform.root.GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_input.shoot)
+        {
+            Shoot();
+            _input.shoot = false;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletPoint.transform.position, bulletPoint.transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+        Destroy(bullet, 1);
+    }
+}
